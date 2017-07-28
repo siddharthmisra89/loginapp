d07246fc34bc6d8603319f8f7eda5c8490c45275
SiddharthMisra <siddharth.misra89@gmail.com>
2017-07-28 18:38:02 +0530

    page modified to login

--- lapp.js
+++ lapp.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+var path = require("path")
+var id = "general"
+var pass = "alladin"
 
-app.get('/', function(req, res){
-  res.send('Hello World!')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/yoyo', function(req, res){
+  if(id == req.body.id && pass == req.body.pass){
+    res.send("alladin motherfucker")
+  } else{
+    res.send("you are HIV alladin")
+  }
 })
+
+
+app.use('/hayorabba', express.static(path.join(__dirname, 'appone')))
+
 
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!')
